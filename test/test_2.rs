befcea4f8de269d6b5c35fc581201f0da9184c26
wu-benjamin <benjaminwu24@gmail.com>
2022-06-21 23:08:27 -0400

    prune examples

--- test/test_2.rs
+++ test/test_2.rs
@@ -1,14 +1,3 @@
-fn minus_one_safe(x: i32) -> i32 {
-    if x > 0 {
-        return x - 1;
-    }
-    return x;
-}
-
-fn minus_one_unsafe(x: i32) -> i32 {
-    return x - 1;
-}
-
 fn neg_abs(mut x: i32) -> i32 {
     if x > 0 {
         x = -1 * x;
@@ -24,8 +13,6 @@ fn abs(mut x: i32) -> i32 {
 }
 
 fn main() {
-    println!("{}", minus_one_safe(3));
-    println!("{}", minus_one_unsafe(3));
     println!("{}", neg_abs(2));
     println!("{}", abs(-2));
 }
--- test/test_3.rs
+++ test/test_3.rs
@@ -1,24 +1,14 @@
-fn unsat_safe_1(x: i32) -> () {
-    assert!(x <= -200 || x >= -300);
-}
-
-fn unsat_safe_2(x: i32) -> () {
+fn unsat_safe(x: i32) -> () {
     assert!(x == x);
 }
 
-fn sat_unsafe_1(x: i32) -> () {
+fn sat_unsafe(x: i32) -> () {
     assert!(x < 13);
 }
 
-fn sat_unsafe_2(x: i32) -> () {
-    assert!(x <= -200 && x >= -300);
-}
-
 fn main() {
-    unsat_safe_1(12);
-    unsat_safe_2(14);
-    sat_unsafe_1(12);
-    sat_unsafe_2(-250);
+    unsat_safe(12);
+    sat_unsafe(-250);
 }
 
 // RUSTC_LOG=rustc_symbolic_exec=debug rustc +stage1 test_3.rs && ./test_3
