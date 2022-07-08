7fd8d487e40b31007d549d65f5189ab9ffbffc06
wu-benjamin <benjaminwu24@gmail.com>
2022-07-08 00:55:51 -0400

    update test 2

--- test/test_2.rs
+++ test/test_2.rs
@@ -1,40 +1,55 @@
-// fn easy_neg_abs(x: i32) -> i32 {
-//     // let y = if x > 0 { -1 * x } else { x };
-//     // assert!(y <= 0);
-//     // return y;
-//     x + 1
+fn test1() -> i32 {
+    let mut x = 2;
+    x = 7;
+    assert!(x == 2);
+    x
+}
+
+fn test2(mut x: i32) -> i32 {
+    x = 2;
+    let y = if x > 0 {
+        -1 * x
+    } else {
+        x
+    };
+    assert!(y <= 0);
+    return y;
+}
+
+// fn test3() -> i32 {
+//     let mut x = 0;
+//     let mut sum = 0;
+//     while x < 10 {
+//         x = x + 1;
+//         sum = sum + x;
+//     }
+//     return sum;
 // }
-// 2147483648
 
-fn minus_one_safe(x: i32) -> i32 {
-    if x > 0 {
-        return x - 1;
-    }
-    return x;
-}
+// fn minus_one_safe(x: i32) -> i32 {
+//     if x > 0 {
+//         return x - 1;
+//     }
+//     return x;
+// }
 
-fn minus_one_unsafe(x: i32) -> i32 {
-    return x - 1;
-}
+// fn minus_one_unsafe(x: i32) -> i32 {
+//     return x - 1;
+// }
 
-fn neg_abs(mut x: i32) -> i32 {
-    if x > 0 {
-        x = -1 * x;
-    }
-    return x;
-}
+// fn neg_abs(mut x: i32) -> i32 {
+//     if x > 0 {
+//         x = -1 * x;
+//     }
+//     return x;
+// }
 
-fn abs(mut x: i32) -> i32 {
-    if x < 0 {
-        x = -1 * x;
-    }
-    return x;
-}
+// fn abs(mut x: i32) -> i32 {
+//     if x < 0 {
+//         x = -1 * x;
+//     }
+//     return x;
+// }
 
 fn main() {
-    // println!("{}", easy_neg_abs(1));
-    println!("{}", minus_one_safe(3));
-    println!("{}", minus_one_unsafe(3));
-    println!("{}", neg_abs(2));
-    println!("{}", abs(-2));
 }
